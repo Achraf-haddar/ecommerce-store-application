221b752953cfd7a49dc354247df241dc4e577b6c
Achraf-haddar <achraf.haddar@supcom.tn>
2024-01-04 12:23:22 +0200

    Add product modal and cart feature

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -5,15 +5,16 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
+  const cart = useCart();
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  const router = useRouter();
 
   if (!isMounted) {
     return null;
@@ -26,7 +27,9 @@ const NavbarActions = () => {
         className="flex items-center rounded-full bg-black px-4 py-2"
       >
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -8,16 +8,31 @@ import { useRouter } from "next/navigation";
 import Currency from "@/components/ui/currency";
 import IconButton from "@/components/ui/icon-button";
 import { Product } from "@/types";
+import PreviewModal from "../preview-modal";
+import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const cart = useCart();
   const router = useRouter();
+  const previewModal = usePreviewModal();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    cart.addItem(data);
+  };
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    previewModal.onOpen(data);
   };
 
   return (
@@ -35,8 +50,12 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
-            <IconButton icon={<Expand size={20} className="text-gray-600" />} />
             <IconButton
+              onClick={onPreview}
+              icon={<Expand size={20} className="text-gray-600" />}
+            />
+            <IconButton
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
